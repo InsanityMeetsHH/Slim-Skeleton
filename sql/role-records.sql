94722fcf134813edeca854624134a6f57a747198
InsanityMeetsHH <software@insanitymeetshh.net>
2018-07-11 12:33:44 +0200

    Fixed sql.

--- sql/role-records.sql
+++ sql/role-records.sql
@@ -1,1 +1,1 @@
-INSERT INTO `role` (`id`, `name`, `deleted`, `updated_at`, `created_at`) VALUES (1, 'guest', 0, now(), now()), (2, 'member', 0, now(), now()), (3, 'admin', 0, now(), now(), (4, 'superadmin', 0, now(), now()));
+INSERT INTO `role` (`id`, `name`, `deleted`, `updated_at`, `created_at`) VALUES (1, 'guest', 0, now(), now()), (2, 'member', 0, now(), now()), (3, 'admin', 0, now(), now()), (4, 'superadmin', 0, now(), now());
